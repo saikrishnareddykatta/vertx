5581e5117d9623d957c970ddffb21b5a1389c9ab
Sai Krishna Reddy Katta <saikrishnareddykatta@Sais-MacBook-Pro-2.local>
2025-12-23 18:30:39 -0700

    feat(vertx-web): scaling the web server - load test

--- vertx-stock-broker/src/main/java/com/learning/udemy/broker/assets/GetAssetsHandler.java
+++ vertx-stock-broker/src/main/java/com/learning/udemy/broker/assets/GetAssetsHandler.java
@@ -8,6 +8,8 @@ import io.vertx.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class GetAssetsHandler implements Handler<RoutingContext> {
   private static final Logger LOG = LoggerFactory.getLogger(GetAssetsHandler.class);
 
@@ -17,9 +19,29 @@ public class GetAssetsHandler implements Handler<RoutingContext> {
       final JsonArray response = new JsonArray();
       AssetsRestApi.ASSETS.stream().map(Asset::new).forEach(response::add);
       LOG.info("Path {} responds with {}", context.normalizedPath(), response.encode());
-      context.response()
+      // Uncomment only when load testing
+     // artificialSleep(context);
+    context.response()
         .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
         .putHeader("my-header", "my-value") // Custom Header
         .end(response.toBuffer());
+  }
+
+  /**
+   * Used to showcase scaling and load testing
+   */
+  private static void artificialSleep(RoutingContext context) {
+    try {
+      // Simulate Delay
+      final var randomMillis = ThreadLocalRandom.current().nextInt(100, 300);
+      if (randomMillis % 2 == 0) {
+        Thread.sleep(randomMillis);
+        context.response()
+          .setStatusCode(500)
+          .end("Sleeping...");
+      }
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
